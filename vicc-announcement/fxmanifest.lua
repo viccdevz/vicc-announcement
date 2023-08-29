605915ee72337fd9ef7a0ef8f3bd17cf63f0f0d7
Vicctory <123509837+viccdevz@users.noreply.github.com>
2023-08-29 22:11:26 +0300

    Add files via upload

---
+++ vicc-announcement/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'viccdevz'
+
+ui_page 'html/index.html'
+
+client_scripts {
+  'client/main.lua',
+}
+
+server_scripts {
+  'server/main.lua'
+}
+
+shared_scripts {
+  'config.lua',
+}
+
+files {
+  'html/*',
+}
+
+
+lua54 'on'
